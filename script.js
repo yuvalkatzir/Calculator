8312379bd2a2304c9279d9d5ba48532f0ddc332d
yuvalkatzir <42411319+yuvalkatzir@users.noreply.github.com>
2022-04-13 22:08:39 +0300

    Finished main functions

---
+++ script.js
@@ -0,0 +1,262 @@
+"use strict"
+
+class Calculator {
+    constructor(storeOperandTextElement,currentOperandTextElement) {
+        this.inputStack = [];
+        this.storeOperandTextElement    = storeOperandTextElement;
+        this.currentOperandTextElement  = currentOperandTextElement;
+        this.clearData();
+        this.updateDisplay();
+    }
+
+    clear(){
+        this.storeOperandTextElement.innerHTML    = 'Ans = ' + this.Ans + ' | x = ' + this.x + ' | y = ' + this.y;
+        this.currentOperandTextElement.innerHTML  = '';
+        this.inputStack = [];
+    }
+
+    delete(){
+        if (this.inputStack.length !== 0) {
+            if (isNaN(this.inputStack[this.inputStack.length - 1]) &&
+                !this.inputStack[this.inputStack.length - 1].includes('.')) {
+                this.inputStack.pop();
+                return;
+            }
+
+            this.inputStack[this.inputStack.length - 1] = this.inputStack[this.inputStack.length - 1].slice(0, -1);
+        }
+    }
+
+    appendNumber(number) {
+        if (isNaN(number)) {
+            if (this.inputStack.length !== 0 && !this.inputStack[this.inputStack.length - 1].includes('.'))
+                this.inputStack[this.inputStack.length - 1] += '.';
+            return;
+        }
+
+       /* if (this.inputStack.length === 0)
+            this.inputStack.push(number);
+        else if (isNaN(this.inputStack[this.inputStack.length - 1]) &&
+            (!(this.inputStack[this.inputStack.length - 1].includes('.')))) {
+            this.inputStack.push(number);
+        } else this.inputStack[this.inputStack.length - 1] += number;*/
+        if (this.inputStack.length === 0)
+            this.inputStack.push(number);
+        else if (isNaN(parseFloat(this.inputStack[this.inputStack.length - 1]))) {
+            this.inputStack.push(number);
+        }
+        else if(this.inputStack[this.inputStack.length - 1] === 'Ans' ||
+            this.inputStack[this.inputStack.length - 1] === 'x' ||
+            this.inputStack[this.inputStack.length - 1] === 'y'){
+            return;
+        } else this.inputStack[this.inputStack.length - 1] += number;
+    }
+
+    chooseOperation(operation){
+        if(this.inputStack.length === 0 && operation !== '-')
+            return;
+        if(this.inputStack.length !== 0 && operation !== '-' && isNaN(this.inputStack[this.inputStack.length - 1])) {
+            if (this.inputStack[this.inputStack.length - 1] === 'Ans' ||
+                this.inputStack[this.inputStack.length - 1] === 'x' ||
+                this.inputStack[this.inputStack.length - 1] === 'y')
+                this.inputStack.push(operation);
+            return;
+        }
+        this.inputStack.push(operation);
+    }
+
+    compute(){
+        if(isNaN(this.inputStack[this.inputStack.length - 1]) &&
+            (this.inputStack[this.inputStack.length - 1] !== 'Ans' &&
+            this.inputStack[this.inputStack.length - 1] !== 'x' &&
+            this.inputStack[this.inputStack.length - 1] !== 'y')){
+            this.inputStack.pop();
+        }
+
+        if(this.inputStack.length === 0)
+            return;
+        if(this.inputStack[0] === '-')
+            this.inputStack.unshift('0');
+
+        this.parser();
+
+        //  calc
+        this.calc('*','÷');
+        this.calc('+','-');
+
+        this.Ans = parseFloat(this.inputStack[0]);
+        this.currentOperandTextElement.innerHTML = this.Ans;
+        this.inputStack = [this.Ans];
+    }
+
+    parser() {
+        for (let i = 0; i < this.inputStack.length; i++) {
+            switch (this.inputStack[i]) {
+                case 'Ans':
+                    this.inputStack[i] = this.Ans;
+                    break;
+                case 'x':
+                    this.inputStack[i] = this.x;
+                    break;
+                case 'y':
+                    this.inputStack[i] = this.y;
+                    break;
+            }
+
+            if (i + 1 < this.inputStack.length) {
+                switch (this.inputStack[i]) {
+                    case '+':
+                        if (this.inputStack[i + 1] === '-') {
+                            this.inputStack = [...this.inputStack.slice(0,i), ...this.inputStack.splice(i + 1)];
+                            this.inputStack[i] = '-';
+                        }
+                        //i = i - 1;
+                        break;
+                    case '-':
+                        if (this.inputStack[i + 1] === '+') {
+                            this.inputStack = [...this.inputStack.slice(0,i), ...this.inputStack.splice(i + 1)];
+                            this.inputStack[i] = '-';
+                        } else if (this.inputStack[i + 1] === '-') {
+                            //let temp = this.inputStack[0];
+                            this.inputStack = [...this.inputStack.slice(0,i), ...this.inputStack.splice(i + 1)];
+                            this.inputStack[i] = '+';
+                        }
+                        //i = i - 1;
+                        break;
+                }
+            }
+        }
+    }
+
+    calc(operator1,operator2){
+        let leftNumber;
+        let rightNumber;
+        let answerStack = [];
+        let currentOperator;
+        let flagCalcNext = false;
+
+        for(let i = 0;i<this.inputStack.length;i++){
+            if(this.inputStack[i] === operator1 || this.inputStack[i] === operator2 || flagCalcNext){
+                if(!flagCalcNext)
+                    flagCalcNext = true;
+                else{
+                    currentOperator = answerStack.pop();
+                    leftNumber      = answerStack.pop();
+                    rightNumber     = this.inputStack[i];
+                    flagCalcNext    = false;
+                    //i=0;
+                    //answerStack= [];
+
+                    switch (currentOperator) {
+                        case '+': answerStack.push(parseFloat(leftNumber) + parseFloat(rightNumber));
+                            break;
+                        case '-': answerStack.push(parseFloat(leftNumber) - parseFloat(rightNumber));
+                            break;
+                        case '*': answerStack.push(parseFloat(leftNumber) * parseFloat(rightNumber));
+                            break;
+                        case '÷': answerStack.push(parseFloat(leftNumber) / parseFloat(rightNumber));
+                            break;
+                    }
+                    for(i = i + 1;i<this.inputStack.length;i++){
+                        answerStack.push(this.inputStack[i]);
+                    }
+                    this.inputStack = answerStack;
+                    answerStack     = [];
+                    i=-1;
+                    continue;
+                }
+            }
+            answerStack.push(this.inputStack[i]);
+        }
+        this.inputStack = answerStack;
+    }
+
+    appendVariable(variable){
+        if(this.inputStack.length !== 0 && (this.inputStack[this.inputStack.length - 1] === 'Ans' ||
+                                            this.inputStack[this.inputStack.length - 1] === 'x'   ||
+                                            this.inputStack[this.inputStack.length - 1] === 'y'))
+            return;
+        this.inputStack.push(variable);
+    }
+
+    store(variable){}
+
+    clearData(){
+        this.x   = 0;
+        this.y   = 0;
+        this.Ans = 0;
+        this.clear();
+    }
+
+    updateDisplay(){
+        let query = '';
+        this.inputStack.forEach(item => {if ((isNaN(item) && item !== '.') ? (query += ' ' + item + ' ') : (query += item));});
+        this.storeOperandTextElement.innerHTML   = 'Ans = ' + this.Ans + ' | x = ' + this.x + ' | y = ' + this.y;
+        this.currentOperandTextElement.innerHTML = query;
+    }
+}
+
+// Buttons
+const buttonNumber     = document.querySelectorAll('[data-number]');
+const buttonClear      = document.querySelectorAll('[data-clear]');
+const buttonOperation  = document.querySelectorAll('[data-operation]');
+const buttonStore      = document.querySelectorAll('[data-store]');
+const buttonEquals     = document.querySelectorAll('[data-equals]');
+const buttonRemoveData = document.querySelectorAll('[data-remove]');
+const buttonDelete     = document.querySelectorAll('[data-delete]');
+const buttonAnswer     = document.querySelectorAll('[data-answer]');
+
+// Text Elements
+const storeOperandTextElement   = document.getElementById("data-store-operand");
+const currentOperandTextElement = document.getElementById("data-current-operand");
+
+const calculator = new Calculator(storeOperandTextElement,currentOperandTextElement);
+
+buttonNumber.forEach(button => {
+    button.addEventListener('click',()=>{
+        calculator.appendNumber(button.innerHTML);
+        calculator.updateDisplay();
+    });
+});
+
+buttonOperation.forEach(button => {
+    button.addEventListener('click',()=>{
+        calculator.chooseOperation(button.innerHTML);
+        calculator.updateDisplay();
+    });
+});
+
+buttonClear.forEach(button => {
+    button.addEventListener('click',()=>{
+        calculator.clear();
+        calculator.updateDisplay();
+    })
+});
+
+buttonRemoveData.forEach(button => {
+    button.addEventListener('click',()=>{
+        calculator.clearData();
+        calculator.updateDisplay();
+    });
+});
+
+buttonDelete.forEach(button=>{
+    button.addEventListener('click',()=>{
+        calculator.delete();
+        calculator.updateDisplay();
+    });
+});
+
+buttonEquals.forEach(button=>{
+    button.addEventListener('click',()=>{
+        calculator.compute();
+        calculator.updateDisplay();
+    });
+});
+
+buttonAnswer.forEach(button=>{
+   button.addEventListener('click',()=>{
+       calculator.appendVariable("Ans");
+       calculator.updateDisplay();
+   });
+});
